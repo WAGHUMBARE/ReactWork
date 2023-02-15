2abf7ab71f0fed449f67ecb5c093cc827c71a2f4
Yash Waghumbare <yash_waghumbare@persistent.com>
2023-02-15 23:30:37 +0530

    Bootstrap_Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button type="button" class="btn btn-success">
+        Success
+      </button>
+      <button type="button" class="btn btn-danger">
+        Danger
+      </button>
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+        </ul>
+        <div class="card-body">
+          <a href="/" class="card-link">
+            Card link
+          </a>
+          <a href="/" class="card-link">
+            Another link
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/count.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Count(prop) {
+  return (
+    <div>
+      <h2>{prop.name}</h2>
+    </div>
+  )
+}
+
+export default Count
