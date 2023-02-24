b1df1360b2c982fb9d18d64b5ea014467404dc1e
Yash Waghumbare <yash_waghumbare@persistent.com>
2023-02-24 17:12:08 +0530

    Updates

---
+++ precache-manifest.9cc9fb2635cb95f844307aa3711484d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2475f2b81875576a8204",
+    "url": "/react-gh-pages/static/css/main.b5f88b8e.chunk.css"
+  },
+  {
+    "revision": "2475f2b81875576a8204",
+    "url": "/react-gh-pages/static/js/main.2475f2b8.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/react-gh-pages/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "37eb08ae3ef68e9ae89e",
+    "url": "/react-gh-pages/static/js/2.37eb08ae.chunk.js"
+  },
+  {
+    "revision": "f4ca0baa3ccb65c1b883",
+    "url": "/react-gh-pages/static/js/runtime~main.f4ca0baa.js"
+  },
+  {
+    "revision": "ccec9e0d7c2d70977d5748c61a6a852b",
+    "url": "/react-gh-pages/index.html"
+  }
+];
